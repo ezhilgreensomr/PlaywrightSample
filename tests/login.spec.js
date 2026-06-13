5b4e30ebeafda612d6bd5a5050bd805f0762c466
ezhilgreensomr <147533004+ezhilgreensomr@users.noreply.github.com>
2026-06-13 10:33:03 +0530

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -41,20 +41,20 @@
 
 
 
-test('login test - invalid user', async ({page})=>{
+// test('login test - invalid user', async ({page})=>{
 
-    await page.goto("https://www.greenstechnologys.com/");
+//     await page.goto("https://www.greenstechnologys.com/");
 
-    const title = await page.title();
-    console.log(title);
+//     const title = await page.title();
+//     console.log(title);
 
-    const url =  page.url();
-    console.log(url);
+//     const url =  page.url();
+//     console.log(url);
 
-    await page.waitForTimeout(3000);
+//     await page.waitForTimeout(3000);
 
-    // await page.locator('//input[@name="email"]').fill('ezhil@gmail.com');
-    let txt = await page.locator("//p[contains(text(),'Training and Placements')]").textContent();
-    console.log(txt);
-    // page.close();
-});
+//     // await page.locator('//input[@name="email"]').fill('ezhil@gmail.com');
+//     let txt = await page.locator("//p[contains(text(),'Training and Placements')]").textContent();
+//     console.log(txt);
+//     // page.close();
+// });
